29c37a0f7e9bac504fe8ceec76e84d9e6c916851
Nehasharma268 <nehasha268@gmail.com>
2025-06-23 20:11:19 +0530

    Project setup and added assets

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+       <div className='pattern'>
+        <div className='wrapper'>
+        <header>
+          <img src="./hero-img.svg" alt="Hero Banner" />
+            <h1>Find <span className='text-gradient'>Movies </span>You'll Enjoy Wihout the Hassle</h1>
+        </header>
+           
+           <p>Search</p>
+        </div>
+       </div>
+    </main>
+  )
+}
+
+export default App
